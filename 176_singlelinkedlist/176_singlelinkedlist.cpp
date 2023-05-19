43878641263cbc5bd013cf7a4fa0a74187ea736a
riskalaluna <riskalaluna@gmail.com>
2023-05-19 15:32:05 +0700

    int main

--- 176_singlelinkedlist/176_singlelinkedlist.cpp
+++ 176_singlelinkedlist/176_singlelinkedlist.cpp
@@ -113,5 +113,40 @@ void searchData() {
 		system("cls");
 		return;
 	}
+	else {
+		int nim;
+		cout << "Masukkan NIM: ";
+		cin >> nim;
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			if (currentNode->noMhs == nim) {
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				return;
+			}
+			currentNode = currentNode->next;
+		}
+		cout << "Data tidak ditemukan" << endl;
+	}
+}
 
+int main() {
+	int pilihan;
+	do
+	{
+		try
+		{
+
+			cout << "1. Tambah Data" << endl;
+			cout << "2. Hapus Data" << endl;
+			cout << "3. Tampilkan Data" << endl;
+			cout << "4. Cari Data" << endl;
+			cout << "5. Keluar" << endl;
+			cout << "Pilihan: ";
+			cin >> pilihan;
+			switch (pilihan)
+			{
+
+			}
+		}
+	}
 }
