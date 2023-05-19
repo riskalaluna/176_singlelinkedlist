390e44313caad80e04931b5b29343442dec2842f
riskalaluna <riskalaluna@gmail.com>
2023-05-19 11:31:58 +0700

    14

--- 176_singlelinkedlist/176_singlelinkedlist.cpp
+++ 176_singlelinkedlist/176_singlelinkedlist.cpp
@@ -57,4 +57,12 @@ bool searchNode(int nim, Node* current, Node* previous) {
 	if (current == NULL) {
 		return false;
 	}
+	else if (current->noMhs == nim) 
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
