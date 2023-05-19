3b352dec01114dc8d8067c6308323909a6ac5ae3
riskalaluna <riskalaluna@gmail.com>
2023-05-19 15:15:24 +0700

    bool listEmpty

--- 176_singlelinkedlist/176_singlelinkedlist.cpp
+++ 176_singlelinkedlist/176_singlelinkedlist.cpp
@@ -78,4 +78,14 @@ bool deleteNode(int nim) {
 	if (searchNode(nim, previous, current) == false)
 		return false;
 	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
+bool listEmpty() {
+	if (START == NULL)
+		return true;
+	else
+		return false;
+
 }
